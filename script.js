c1a78b9d1d4b7016b125e2db47d2988ec8f4eff9
devmedra <devmedra@gmail.com>
2026-01-05 13:14:18 -0300

    1328

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Hola desde JS!");
+
+// Agrega un botón en HTML: <button id="miBoton">Haz clic</button>
+document.getElementById("miBoton").addEventListener("click", function() {
+    alert("¡Botón clicado!");
+});
